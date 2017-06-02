7dc9e071dc308a6a08e05adc73f951945b61445e
walinchus <Lucia@professionalnewsservices.com>
2017-06-02 12:00:59 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,29 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import urllib2
+import lxml.etree
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+pdfdata = urllib2.urlopen("https://www.ok.gov/osbi/documents/Crime%20in%20Oklahoma%202016%20Final%205.26.17.pdf").read()
+#...Then using pdftoxml to convert that into a variable called 'xmldata'
+                xmldata = scraperwiki.pdftoxml(pdfdata)
+#...Then using .fromstring to convert that into a variable called 'pdfxml'
+                pdfxml = lxml.etree.fromstring(xmldata)
+                print xmldata
+    
+#def scrape_and_look_for_next_link(url):
+    #scrapes the page and puts it in 'html'
+   # html = scraperwiki.scrape(url)
+    #print html
+    #turns html from a string into an lxml object called 'root'
+    #root = lxml.html.fromstring(html)
+    #runs another function - created earlier - on 'root'
+    #scrapetable(root)
+
+#This will be used for relative links in later pages
+#baseurl = "https://www.ok.gov"
+#When added to the baseurl, this is our starting page 
+#startingurl = "/osbi/documents/Crime%20in%20Oklahoma%202016%20Final%205.26.17.pdf
+
+#Run the function created earlier above on that URL
+#scrape_and_look_for_next_link(baseurl+startingurl)
