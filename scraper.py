ab448b28444750aaf3436b347531c2edaf3d0b12
walinchus <Lucia@professionalnewsservices.com>
2017-06-02 12:03:50 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,10 +6,10 @@ import lxml.html
 
 pdfdata = urllib2.urlopen("https://www.ok.gov/osbi/documents/Crime%20in%20Oklahoma%202016%20Final%205.26.17.pdf").read()
 #...Then using pdftoxml to convert that into a variable called 'xmldata'
-                xmldata = scraperwiki.pdftoxml(pdfdata)
+xmldata = scraperwiki.pdftoxml(pdfdata)
 #...Then using .fromstring to convert that into a variable called 'pdfxml'
-                pdfxml = lxml.etree.fromstring(xmldata)
-                print xmldata
+pdfxml = lxml.etree.fromstring(xmldata)
+print xmldata
     
 #def scrape_and_look_for_next_link(url):
     #scrapes the page and puts it in 'html'
